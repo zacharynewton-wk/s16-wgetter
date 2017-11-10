705acc45e7ef198d8c69b9ed7b2ca2014c77ebc3
zacharynewton-wk <zachary.newton@workiva.com>
2017-11-10 10:59:07 -0600

    Placeholder app stuff to see it work

--- main.js
+++ main.js
@@ -6,18 +6,12 @@ let win;
 
 function createWindow () {
     win = new BrowserWindow({width: 800, height: 600});
-
-    win.loadUrl(url.format({
+    win.loadURL(url.format({
         pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
         slashes: true
     }))
-
-    win.webContents.openDevTools()
-
-    win.on('closed', () => {
-        win = null;
-    })
+    win.on('closed', () => {win = null;})
 }
 
 app.on('ready', createWindow);
